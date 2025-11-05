f04aa0b54e98c2ea62022c72185834bda9663125
maxime64100 <maximefallek1@gmail.com>
2025-11-05 09:38:24 +0100

    add pages

---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,8 @@
+export function Favorites() {
+  return (
+    <div>
+      <h1>Favoris</h1>
+      <p>Bienvenue sur la page des favoris qui permet de voir vos jeux vidéos préférés !</p>
+    </div>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export function Home() {
+  return (
+    <div>
+      <h1>Accueil</h1>
+      <p>Bienvenue sur la page d'accueil qui permet de rechercher des jeux vidéos !</p>
+      <input type="text" placeholder="Rechercher un jeu vidéo..." />
+    </div>
+  );
+}
